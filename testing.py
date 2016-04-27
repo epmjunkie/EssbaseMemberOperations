69599701a068e6b444b973e55e590810d102bc4e
Keith Kikta <kkikta@gmail.com>
2016-04-27 13:35:58 -0500

    updated to reflect changes in delete function

--- testing.py
+++ testing.py
@@ -5,7 +5,7 @@ conn = essbase(username="admin", password="password", server="essbase.server.net
 otl = outline(essbase=conn, application="Sample", database="Basic")
 conn.open()
 otl.open()
-status, message = otl.delete1(member='TAXRATE', operation=otl.Operation.Level0)
+status, message = otl.delete(member='TAXRATE', operation=otl.Operation.Level0)
 print(status)
 print(message)
 otl.save()
